6c129503c2c5267fb3c837feeea79e9f19d07698
Pallavi_270 <veeramanikantapallavi@gmail.com>
2025-01-05 21:03:46 +0530

    second

---
+++ scripts/navbar.js
@@ -0,0 +1,5 @@
+const navbar=`<img src=VegaPoint.png/>
+<input id="in"type="text" placeholder="search for product">
+<button id="location">Select Location</button>
+<button id="signup">Login/Signup</button> `;
+    document.getElementById("nav").innerHTML=navbar
